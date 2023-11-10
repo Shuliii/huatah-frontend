b2636f79fd4d737dfe9100d3bfa9e5644e91beff
Shuliii <ricoharsono@gmail.com>
2023-11-10 23:26:04 +0800

    edit

--- src/components/BetItem.js
+++ src/components/BetItem.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
-import AddToCartButton from "./UI/AddToCartButton";
-
 import styles from "./BetItem.module.css";
-import { GoTriangleUp } from "react-icons/go";
+import { motion, AnimatePresence } from "framer-motion";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 const BetItem = ({ item }) => {
   const [isExpand, setIsExpand] = useState(false);
@@ -15,7 +14,11 @@ const BetItem = ({ item }) => {
     });
   };
   return (
-    <li className={styles.listitem}>
+    <motion.li
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      className={styles.listitem}
+    >
       <div className={styles.matchname}>
         <div>{item.match_name}</div>
         <div>date</div>
@@ -23,38 +26,57 @@ const BetItem = ({ item }) => {
       <div className={styles.betlistmain}>
         {item.main_bet_list.map((item) => {
           return (
-            <div className={styles.eachBet}>
+            <div key={Math.random()} className={styles.eachBet}>
               <div>{item.name}</div>
               <div>{item.odds}</div>
-              <AiFillPlusCircle size={12} className={styles.plus} />
+              <AiOutlinePlusCircle
+                size={12}
+                className={styles.plus}
+                color={"red"}
+              />
             </div>
           );
         })}
       </div>
       <div className={styles.seemore}>
         <span onClick={seeMoreHandler}>
-          See more
+          See {isExpand ? "less" : "more"}
           {/* <span className={styles.triangleIcon}>
             <GoTriangleUp />
           </span> */}
         </span>
       </div>
-      {isExpand && (
-        <div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-          <div>test</div>
-        </div>
-      )}
-    </li>
+      <AnimatePresence>
+        {isExpand && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            className={styles.betlistmore}
+          >
+            {item.more_bet_list.map((item) => {
+              return (
+                <motion.div
+                  initial={{ opacity: 0, y: -80 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, type: "spring" }}
+                  key={Math.random()}
+                  className={styles.eachBet}
+                >
+                  <div>{item.name}</div>
+                  <div>{item.odds}</div>
+                  <AiFillPlusCircle
+                    size={12}
+                    className={styles.plus}
+                    color={"red"}
+                  />
+                </motion.div>
+              );
+            })}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.li>
   );
 };
 
