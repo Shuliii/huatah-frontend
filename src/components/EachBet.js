36b01ffdf07f5b5872cd4bc410b2476757cb923b
Shuliii <ricoharsono@gmail.com>
2023-11-11 11:00:30 +0800

    edit

--- src/components/EachBet.js
+++ src/components/EachBet.js
@@ -1,12 +1,22 @@
 import { useState, memo } from "react";
+import { useSelector } from "react-redux";
+
+import Modal from "./UI/Modal";
+
 import styles from "./EachBet.module.css";
 import { motion } from "framer-motion";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 
 const EachBet = ({ item, onClick }) => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const [addedToCart, setAddedToCart] = useState(false);
+  const [hasError, setError] = useState("");
 
   const plusClickHandler = () => {
+    if (!isLoggedIn) {
+      setError("Please LogIn before placing any bets!");
+      return;
+    }
     const data = {
       name: item.name,
       odds: item.odds,
@@ -21,33 +31,43 @@ const EachBet = ({ item, onClick }) => {
     setAddedToCart(false);
   };
 
+  const closeHandler = () => {
+    setError("");
+  };
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -80 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, type: "spring" }}
-      key={Math.random()}
-      className={styles.eachBet}
-    >
-      <div>{item.name}</div>
-      <div>{item.odds}</div>
-      {!addedToCart && (
-        <AiOutlinePlusCircle
-          size={12}
-          className={styles.plus}
-          color={"red"}
-          onClick={plusClickHandler}
-        />
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: -80 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+        className={styles.eachBet}
+      >
+        <div>{item.name}</div>
+        <div>{item.odds}</div>
+        {!addedToCart && (
+          <AiOutlinePlusCircle
+            size={12}
+            className={styles.plus}
+            color={"red"}
+            onClick={plusClickHandler}
+          />
+        )}
+        {addedToCart && (
+          <AiOutlineMinusCircle
+            size={12}
+            className={styles.minus}
+            color={"red"}
+            onClick={minusClickHandler}
+          />
+        )}
+      </motion.div>
+      {hasError && (
+        <Modal onClose={closeHandler}>
+          <div className={styles.modalContent}>{hasError}</div>
+        </Modal>
       )}
-      {addedToCart && (
-        <AiOutlineMinusCircle
-          size={12}
-          className={styles.minus}
-          color={"red"}
-          onClick={minusClickHandler}
-        />
-      )}
-    </motion.div>
+    </>
   );
 };
 
