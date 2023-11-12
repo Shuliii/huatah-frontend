f092c45396417528b8c777009d5e2211c2a0fb3a
Shuliii <ricoharsono@gmail.com>
2023-11-12 18:58:48 +0800

    edit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import Modal from "./UI/Modal";
+import Button from "./UI/Button";
 
 import styles from "./Cart.module.css";
 
@@ -26,6 +27,17 @@ const Cart = ({ onClose }) => {
       <div className={styles.modalContent}>
         {cart.length > 0 && <ul>{liHelper}</ul>}
         {cart.length === 0 && <div>No Items in the cart!</div>}
+
+        <div className={styles.buttonContainer}>
+          <button
+            type="button"
+            onClick={onClose}
+            className={styles.buttonCancel}
+          >
+            Cancel
+          </button>
+          {cart.length > 0 && <Button>Submit</Button>}
+        </div>
       </div>
     </Modal>
   );
